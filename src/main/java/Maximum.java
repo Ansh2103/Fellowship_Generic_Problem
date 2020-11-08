dd8e7dfa5e2d0944ab3db8059c2ab90c8f10e534
Ansh2103 <swayamshubham007.sm@gmail.com>
2020-11-08 05:59:35 -0800

    Added : Functionality to refactor to add generic

--- src/main/java/Maximum.java
+++ src/main/java/Maximum.java
@@ -1,23 +1,8 @@
 public class Maximum {
-    public int findMax(Integer fValue, Integer sValue, Integer tValue) {
 
-        Integer max = fValue;
-
-        if(sValue.compareTo(max) > 0)
-        {
-            max = sValue;
-        }
-        if(tValue.compareTo(max) >0)
-        {
-            max = tValue;
-        }
-        return max;
-    }
-    public Float findMax(Float fValue, Float sValue, Float tValue) {
-
-        Float max = fValue;
-
-        if(sValue.compareTo(max) > 0)
+    public static  <T extends Comparable<T>> T findMax( T fValue,T sValue ,T tValue) {
+        T max = fValue;
+        if(sValue.compareTo(max) > 0 )
         {
             max = sValue;
         }
@@ -28,18 +13,4 @@ public class Maximum {
         return max;
     }
 
-    public String findMax( String fValue, String sValue, String tValue) {
-
-        String max = fValue;
-
-        if(sValue.compareTo(max) > 0)
-        {
-            max = sValue;
-        }
-        if(tValue.compareTo(max) >0)
-        {
-            max = tValue;
-        }
-        return max;
-    }
 }
